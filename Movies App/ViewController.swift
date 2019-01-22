2262b7f059191ff033874d192582c85b1162e9b3
iyinraphael <iyinraphael@gmail.com>
2019-01-22 10:35:21 -0800

    initial commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Iyin Raphael on 1/22/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
