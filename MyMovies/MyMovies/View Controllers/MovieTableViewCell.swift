15dcc97850220bf9910fef52aae30bdc6bb53cc6
iyinraphael <iyinraphael@gmail.com>
2019-01-25 08:50:04 -0800

    custom cells up and running

---
+++ MyMovies/MyMovies/View Controllers/MovieTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Iyin Raphael on 1/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBAction func addMovie(_ sender: UIButton) {
+    }
+    
+}
---
+++ MyMovies/MyMovies/View Controllers/MyMovieTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MyMovieTableViewCell.swift
+//  MyMovies
+//
+//  Created by Iyin Raphael on 1/25/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class MyMovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var hasWatched: UIButton!
+}
